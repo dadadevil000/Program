7eaf2f3025aa25afbe6923072e07d20d5d511662
Dada Devil <dadadevil000@gmail.com>
2023-03-08 02:06:09 -0800

    print my name

--- Demo.java
+++ Demo.java
@@ -1,11 +1,6 @@
-import java.util.Scanner;
-
 class Demo{
     public static void main(String[] args)
     {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter your name");
-        String a = s.nextLine();
-        System.out.println(a);
-    }
+   System.out.println("Makarand Hinge");
+}
 }
